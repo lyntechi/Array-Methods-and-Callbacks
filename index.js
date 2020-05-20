4f43ddb767e45ad64f2a1554121ee40425ef7ee4
lyntechi <lynda-santiago@lambdastudents.com>
2020-05-19 20:34:52 -0400

    task 1 completed

--- index.js
+++ index.js
@@ -1,25 +1,57 @@
 import { fifaData } from './fifa.js';
 console.log(fifaData);
 
-console.log('its working');
+// console.log('its working');
 // ⚽️ M  V P ⚽️ //
 
-/* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
+//  Task 1: Investigate the data above. Practice accessing data by
+//  console.log-ing the following pieces of data 
 
-(a) Home Team name for 2014 world cup final
-(b) Away Team name for 2014 world cup final
-(c) Home Team goals for 2014 world cup final
-(d) Away Team goals for 2014 world cup final
-(e) Winner of 2014 world cup final */
+// (a) Home Team name for 2014 world cup final
+// (b) Away Team name for 2014 world cup final
+// (c) Home Team goals for 2014 world cup final
+// (d) Away Team goals for 2014 world cup final
+// (e) Winner of 2014 world cup final */
 
 
-/* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
+let result = fifaData.filter(function(element){
+    
 
-function getFinals(/* code here */) {
+ return element.Year === 2014 && element.Stage === "Final";
+    
+});
+// console.log(result);
+console.log(result[0]["Home Team Name"]);
+console.log(result[0]["Away Team Name"]);
+console.log(result[0]["Home Team Goals"]);
+console.log(result[0]["Away Team Goals"]);
+console.log(result[0]["Win conditions"]);
 
-    /* code here */
 
+
+
+/* Task 2: Create a function called  getFinals that takes `data` as an argument and returns 
+an array of objects with only finals data */
+
+function getFinals(data) {
+     
+let getFinals = data.filter(function data{
+    return data.Stage["Final"]
+});
+    
+console.log(getFinals(fifaData(data)))
 };
+
+
+
+
+
+// function getYears(getFinals) {
+//     let years = getFinals.map(function getFinals {
+//         return years = [];
+//     });
+//     console.log(years);
+// };
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
